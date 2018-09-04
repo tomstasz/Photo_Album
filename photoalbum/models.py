4b7ef41ec4fee05e97a4f76ed6292b543b1f4485
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2018-09-04 15:49:46 +0200

    create photo model and basic template

--- photoalbum/models.py
+++ photoalbum/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Photo(models.Model):
+    path = models.CharField(max_length=64, verbose_name='ścieżka')
+    creation_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- photoalbum/views.py
+++ photoalbum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.response import TemplateResponse
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'photoalbum/index.html', {'message': 'Witaj świecie'})
